1acdcf1c602221e07db1a6158c69920d50b8a98f
NikitaQaTest <nikita.wolodin@gmail.com>
2023-10-28 14:58:42 +0300

    123

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,22 +1,30 @@
 import { defineConfig } from "cypress";
 
-export default defineConfig({
-  //reporter: 'mochawesome',
-  reporterOptions: {
-    reportDir: 'cypress/results',
-    overwrite: false,
-    html: false,
-    json: true,
-  },
-  viewportHeight: 1080,
-  viewportWidth: 1920,
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+module.exports = {
+  projectId: "zonr7a",
+  // ...rest of the Cypress project config
+};
+
+  export default defineConfig({
+    //reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/results',
+      overwrite: false,
+      html: false,
+      json: true,
     },
-    specPattern: "cypress/e2e/**/*.spec.{js,jsx,ts,tsx}",  //образец файлов .spec.
-    excludeSpecPattern: ["**/2-advanced-exampless"],  //эти файлы не рассматриваются 
-    baseUrl: "http://localhost:4200/",
-    defaultCommandTimeout: 4000,
-  },
-});
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    e2e: {
+      setupNodeEvents(on, config) {
+        // implement node event listeners here
+      },
+      specPattern: "cypress/e2e/**/*.spec.{js,jsx,ts,tsx}",  //образец файлов .spec.
+      excludeSpecPattern: ["**/2-advanced-exampless"],  //эти файлы не рассматриваются 
+      baseUrl: "http://localhost:4200/",
+      defaultCommandTimeout: 4000,
+    },
+  });
+
+
+
